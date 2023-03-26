4b1cb8fe54405a4283de223d1c3baa56c16ffc4f
Michael Jeung <michaelj229@gmail.com>
2023-03-26 11:20:20 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+    .then(res => res.json())
+    .then(json => renderBooks(json))
+return fetch("https://anapioficeandfire.com/api/books");
 }
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
+function renderBooks(json) {
+  const main = document.querySelector("main");
+  json.forEach(book => {
+    const h2 = document.createElement("h2");
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function() {
   fetchBooks();
 });
